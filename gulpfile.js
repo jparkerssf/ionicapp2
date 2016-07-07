0055c29c3d823e2221b04df456860e5a96a88f54
jparkerssf <jparker@softstackfactory.org>
2016-07-07 18:30:36 +0000

    testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,4 @@
 var gulp = require('gulp');
-
-
-var Server = require('karma').Server;
-/**
-* Test task, run test once and exit
-*/
-gulp.task('test', function(done) {
-  new Server({
-    configFile: __dirname + '/tests/my.conf.js',
-    singleRun: true,
-    autoWatch: false
-  }, done).start();
-});
 // var gutil = require('gulp-util');
 // var bower = require('bower');
 // var concat = require('gulp-concat');
@@ -62,3 +49,14 @@ gulp.task('test', function(done) {
 //   }
 //   done();
 // });
+var Server = require('karma').Server;
+/**
+* Test task, run test once and exit
+*/
+gulp.task('test', function(done) {
+  new Server({
+    configFile: __dirname + '/tests/my.conf.js',
+    singleRun: true,
+    autoWatch: false
+  }, done).start();
+});
